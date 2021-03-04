5c3640d858a5b58e2935e128d4608f4e3c7b0cdd
FikriNurhakimAkbar <nurhakimf2906@example.com>
2021-03-04 22:52:52 +0700

    menyelesaikan pertemuan1 sampai pertemuan3 dan praktikum

---
+++ pertemuan1/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Fikri Nurhakim Akbar
+203040170
+Teknik Informatika (D)
+Jumat, 26 Februari 2021
+Pemrograman Web
+Menuliskan Hello World
+*/
+
+?>
+
+<?php
+
+echo "Hello World!";
+
+?>
---
+++ pertemuan3/latihan3.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+---------------------------
+Fikri Nurhakim Akbar
+---------------------------
+203040170
+---------------------------
+Teknik Informatika (D)
+---------------------------
+Minggu, 28 Februari 2021
+---------------------------
+Pemrograman Web
+---------------------------
+Pertemuan 3 - Struktur Kendali
+---------------------------
+*/
+
+?>
+
+<?php 
+
+/* 
+---------------------------
+Pengkondisian / Percabangan
+---------------------------
+if else, if else if else, ternary, switch 
+---------------------------
+*/
+
+$x = 20;
+if( $x < 20 ) {
+    echo "Benar";
+}
+else if( $x == 20 ) {
+    echo "Bingo";
+} 
+else {
+    echo "Salah";
+}
+
+?>
